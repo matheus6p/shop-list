9c266764aa080b7918bca4f6a6cb69ba0f462cbd
matheus6p <mhpaes97@gmail.com>
2022-06-12 23:46:47 -0300

    initial commit

--- App.js
+++ App.js
@@ -1,15 +1,6 @@
 import { View, Text } from "react-native";
+import { WebView } from "react-native-webview";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
+  return <WebView source={{ uri: "https://shop-list-rho.vercel.app/" }} />;
 }
